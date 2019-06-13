06425b39888b09da2bbd1b7be3dbc55cbcffda24
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-06-13 14:23:39 -0500

    updated basics java

--- Basics.java
+++ Basics.java
@@ -1,37 +1,87 @@
+import java.util.ArrayList;
 public class Basics{
     public void printNum(int start, int end){
-        for (var i=start;i<= end;i++){
+        for (int i=start;i<= end;i++){
             System.out.println(i);
         }
     }
     public void printOdd(int end){
-        for (var i=1;i<= end;i+=2){
+        for (int i=1;i<= end;i+=2){
             System.out.println(i);
         }
     }
     public void printSum(int end){
         int sum = 0;
-        for (var i=0;i<= end;i+=2){
+        for (int i=0;i<= end;i++){
             sum += i;
-            System.out.println(i + "sum is: " + sum);
+            System.out.println(i + "  sum is: " + sum);
         }
     }
     public void printArr(int Arr []){
         int sum = 0;
-        for (var i=0;i<= Arr.length;i+=2){
+        for (int i=0;i< Arr.length;i++){
             System.out.println(Arr[i]);
         }
     }
-    public Integer printMax(int Arr []){
+    public void printMax(int Arr []){
         int Max = Arr[0];
-        for (var i=0;i<= Arr.length;i+=2){
+        for (int i=0;i< Arr.length;i++){
             if (Arr[i]> Max){
                 Max = Arr[i];
             }
         }
-        return Max;
+        System.out.println("Max is: " + Max);
     }
-    
+    public void getAvg(int Arr []){
+        int sum = 0;
+        for (int i=0;i< Arr.length;i++){          
+            sum += Arr[i];
+        }
+        System.out.println("Average is: " + (sum/Arr.length));
+    }
+    public void ArrOdd(int end){
+        ArrayList<Integer> Arr = new ArrayList<Integer>();
+        for (int i=1;i<= end;i+=2){
+           Arr.add(i);
+        }
+        System.out.println(Arr);
+    }
+    public void greaterThanY(int Arr [], int y){
+        int val =0;
+        for (int i=0;i< Arr.length;i++){
+            if (Arr[i] > y){
+                val++;
+            }
+        }
+        System.out.println(val);
+    }
+    public void squareArr (int Arr []){
+        for (int i=0;i<Arr.length;i++){
+            Arr[i]=Arr[i]*Arr[i];
+        }
+    }
+    public void replaceNegatives (int Arr []){
+        for (int i=0;i<Arr.length;i++){
+            if (Arr[i]<0){
+                Arr[i] = 0;
+            }
+            System.out.println("newVal at arr[" + i + "]is: " + Arr[i]);  
+        }
+    }
+    public int[] MaxMinAvg (int Arr[]){
+        int[] newArr = {Arr[0],Arr[0],0};
+        for (int i = 0; i< Arr.length; i++){
+            if (newArr[0]<Arr[i]){
+                newArr[0]=Arr[i];
+            }
+            if (newArr[1]>Arr[i]){
+                newArr[1]=Arr[i];
+            }
+            newArr[2] += Arr[i];
+        }
+        newArr[2] /= Arr.length;
+        return newArr;
+    }
 
 
 
--- BasicsTest.java
+++ BasicsTest.java
@@ -1,5 +1,46 @@
 public class BasicsTest{
     public static void main(String[] args) {
-        
+        Basics test = new Basics();
+        // Part 1
+        System.out.println("Part 1: ");
+        test.printNum(1,255);
+        // Part 2
+        System.out.println("Part 2: ");
+        test.printOdd(255);
+        // Part 3
+        System.out.println("Part 3: ");
+        test.printSum(255);
+        // Part 4
+        System.out.println("Part 4: ");
+        int[] arr = {1,2,3,4};
+        test.printArr(arr);
+        // Part 5
+        System.out.println("Part 5: ");
+        test.printMax(arr);
+        // Part 6
+        // different array is being used 
+        System.out.println("Part 6: ");
+        int[] arr1 = {10,2,3};
+        test.getAvg(arr1);
+        //Part 7
+        System.out.println("Part 7: ");
+        test.ArrOdd(255);
+        // Part 8
+        System.out.println("Part 8: ");
+        test.greaterThanY(arr, 2);
+        // Part 9
+        System.out.println("Part 9: ");
+        System.out.println("Non-squared array: " + arr1[0] + " , " + arr1[1] + " , " + arr1[2]);
+        test.squareArr(arr1);
+        System.out.println("squared array: " + arr1[0] + " , " + arr1[1] + " , " + arr1[2]);
+        // Part 10 
+        System.out.println("Part 10: ");
+        int[] arr3 = {1,2,4, -1,-2,-4};
+        test.replaceNegatives(arr3);
+        // Part 11 
+        System.out.println("Part 11: ");
+        int[] arr4 = {1,2,4, -1,-2,-4,5,22};
+        int[] arr5 = test.MaxMinAvg(arr4);
+        System.out.println("Max is: " + arr5[0] + " , Min is:" + arr5[1] + " , Avg is: " + arr5[2]);
     }
 }
